0777e29df12a1c57a1a4e63cc23d49b73563aaeb
twenty-one-savage <alohafrombali@gmail.com>
2021-01-18 19:37:41 +0300

    frontops

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import ReactDOM from "react-dom";
+import App from "./components/commons/app/app";
+import {createStore, applyMiddleware} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension";
+import {Provider} from "react-redux";
+import thunk from "redux-thunk";
+// import {createAPI} from "./services/api/api";
+// import {requiredAuthorization} from "./store/action";
+// import {AuthorizationStatus} from "./consts";
+// import rootReducer from "./store/reducers/root-reducer";
+// import {redirect} from "./store/middlewares/redirect";
+
+// const api = createAPI(() => store.dispatch(requiredAuthorization(AuthorizationStatus.NO_AUTH)));
+
+// const store = createStore(
+//     rootReducer,
+//     composeWithDevTools(
+//         applyMiddleware(thunk.withExtraArgument(api)),
+//         applyMiddleware(redirect),
+//     ),
+// );
+
+ReactDOM.render(
+    // <Provider store={store}>
+    <App/>
+    // </Provider>,
+    document.querySelector(`#root`),
+);
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        open: true,
+        port: 1337,
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            }
+        ],
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            React: 'react',
+            PropTypes: 'prop-types'
+        })
+    ],
+    resolve: {
+        modules: ['node_modules', 'src'],
+        extensions: ['.js', '.jsx']
+    },
+    devtool: 'source-map',
+};
